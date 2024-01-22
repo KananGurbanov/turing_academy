dc63b77138df62bc8e84c6d5906fb57df2e5f5ce
TheCoteen <knanqurbanov22@gmail.com>
2024-01-22 19:01:03 +0400

    tezekanan

---
+++ src/main/java/fesil.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class fesil {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        byte k = -128;
+
+        byte b = (byte) 129;
+
+        int num = 130;
+
+        
+
+        System.out.println(-k);
+
+
+    }
+}
