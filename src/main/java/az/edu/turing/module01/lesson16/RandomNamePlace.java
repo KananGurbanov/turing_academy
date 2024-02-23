7212203e519cc99057cc3def923f8747dfca563b
TheCoteen <knanqurbanov22@gmail.com>
2024-02-23 18:46:46 +0400

    tezekanan

---
+++ src/main/java/az/edu/turing/module01/lesson16/RandomNamePlace.java
@@ -0,0 +1,36 @@
+package az.edu.turing.module01.lesson16;
+
+import java.util.Random;
+
+public class RandomNamePlace {
+    public static void main(String[] args) {
+
+
+        String[] names = {"Kanan", "Ilham(o7)", "Nurlan", "Aydan", "Selen", "Farid K", "Aybeniz", "Nazrin", "Bashir", "Vusal", "Farid H", "Osman", "Eldar", "Bahruz", "Taleh", "Ali", "Leyla"};
+
+        shuffleNames(names);
+
+
+        for (int i = 0; i < names.length; i++) {
+            int randomNumber = i + 1;
+            System.out.println(randomNumber + ". " + names[i]);
+        }
+    }
+
+    public static void shuffleNames(String[] names) {
+        Random rand = new Random();
+
+
+        for (int i = names.length - 1; i > 0; i--) {
+
+            int randomIndex = rand.nextInt(i + 1);
+
+            String temp = names[i];
+            names[i] = names[randomIndex];
+            names[randomIndex] = temp;
+        }
+    }
+
+
+
+}
