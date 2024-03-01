0d8c9cfae8871f6600eca7145fc6c0b9f948bad2
TheCoteen <knanqurbanov22@gmail.com>
2024-03-01 20:27:03 +0400

    hello

--- src/main/java/az/edu/turing/module01/lesson9/TestApp.java
+++ src/main/java/az/edu/turing/module01/lesson9/TestApp.java
@@ -1,20 +1,24 @@
 package az.edu.turing.module01.lesson9;
 
+import java.util.Scanner;
+
 public class TestApp {
     public static void main(String[] args) {
-        func("qebul");
+        Scanner s = new Scanner(System.in);
+        String str = s.next();
+        func(str);
     }
 
     public static void func(String str){
-        if(str.equals("qebul")){
+        if(str.equalsIgnoreCase("qebul")){
             System.out.println("Sifaris qebul olundu");
         }
-        else if(str.equals("gelir")){
+        else if(str.equalsIgnoreCase("gelir")){
             System.out.println("Sifaris yoldadir");
         }
-        else if (str.equals("catdirildi")){
+        else if (str.equalsIgnoreCase("catdirildi")){
             System.out.println("Sifaris catdirildi");
-        } else if (str.equals("legv")) {
+        } else if (str.equalsIgnoreCase("legv")) {
             System.out.println("Sifaris legv olundu");
         }
 
