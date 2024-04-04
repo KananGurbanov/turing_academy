51bd2300d1714b9fae9faba06a908f96024dcf6e
TheCoteen <knanqurbanov22@gmail.com>
2024-04-04 14:12:09 +0400

    hello

---
+++ src/main/java/az/edu/turing/module00/lessonSwitch/SwitchCase.java
@@ -0,0 +1,48 @@
+package az.edu.turing.module00.lessonSwitch;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        int x = 6;
+        switch (x){
+            case 8:
+                System.out.println("hello");
+
+
+            case 5,6:
+                System.out.println("Halllo");
+
+            case 7:
+                System.out.println("Halo");
+            default:
+                System.out.println("heloo");
+        }
+
+
+        switch (x){
+            case 8:
+                System.out.println("hello");
+
+
+            case 5:
+                System.out.println("Halllo");
+
+            case 7:
+                System.out.println("Halo");
+            default:
+                System.out.println("heloo");
+        }
+
+
+        //switch expression
+        String helloType = switch (x){
+            case 8 -> "Halloo";
+            case 5 ->"hello";
+            default -> "no greeting";
+
+        };
+
+        System.out.println(helloType);
+
+    }
+}
