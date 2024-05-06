55f601a7aa73f3ac7abb0e6d1772e2f9edf65d0e
TheCoteen <knanqurbanov22@gmail.com>
2024-05-06 17:29:46 +0400

    hello

--- src/main/java/az/edu/turing/module01/part2/lesson10/Pointers.java
+++ src/main/java/az/edu/turing/module01/part2/lesson10/Pointers.java
@@ -4,18 +4,6 @@ public class Pointers {
     public static void main(String[] args) {
         String a = "kenan", b = "ferid";
 
-        System.out.printf("a = %s, b = %s\n", a, b);
-
-        swap(a,b);
-
-        System.out.printf("a = %s, b = %s", a, b);
-
-        final int k = 7;
-
-        String m = "sdfs";
-
-        m.concat("dsds");
-        System.out.println(m);
 
 
 
